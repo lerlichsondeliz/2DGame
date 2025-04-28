400c0a56cf12fcf4fa16f10399064d5da166e58c
Katherine Freeman <kfreeman@ithaca.edu>
2025-04-28 04:27:48 -0400

    health bar moves when either player takes damage

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -6,7 +6,27 @@ using UnityEngine.SceneManagement;
 public class Level : MonoBehaviour {
 
 
-    public void firstLevel() {
-        
+    public PlayerHealth player1Health; 
+    public PlayerHealth player2Health; 
+    private Slider healthBarSlider;
+    private float combinedMaxHealth;
+
+    void Start()
+    {
+        combinedMaxHealth = player1Health.maxHealth + player2Health.maxHealth;
+        healthBarSlider.maxValue = combinedMaxHealth;
+
+            // Set the initial value to the combined max health
+        healthBarSlider.value = combinedMaxHealth;
     }
+
+     void Update()
+    {
+        if (healthBarSlider != null && player1Health != null && player2Health != null)
+        {
+            // Calculate the combined current health
+            float combinedCurrentHealth = player1Health.CurrentHealth + player2Health.CurrentHealth;
+            healthBarSlider.value = combinedCurrentHealth;
+        }
+}
 }

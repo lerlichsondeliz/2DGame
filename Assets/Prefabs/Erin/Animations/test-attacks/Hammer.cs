ef41feffbadfa66a43233ee1d0c7dd2982892c58
Erin <123580763+erinren210@users.noreply.github.com>
2025-05-05 05:08:09 -0400

    testing attack animations!

---
+++ Assets/Prefabs/Erin/Animations/test-attacks/Hammer.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using System.Collections;
+
+public class Hammer : Weapon
+{
+    public enum PlayerID { Player1, Player2 }
+    public PlayerID owner = PlayerID.Player1;
+
+    public float innerRange = 1f;
+    public float outerRange = 2f;
+    public float outerDamageMultiplier = 0.5f;
+    public float stunDuration = 1.5f;
+    private Animator animator;
+    
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (owner == PlayerID.Player1 && Input.GetKeyDown(KeyCode.E))
+        {
+            Attack();
+            animator.SetTrigger("HammerTrigger");
+        }
+        else if (owner == PlayerID.Player2 && Input.GetKeyDown(KeyCode.O))
+        {
+            Attack();
+            animator.SetTrigger("HammerTrigger");
+        }
+    }
+
+    public override void Attack()
+    {
+        Debug.Log(owner + " hammer attack triggered!");
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position, outerRange);
+
+        foreach (Collider2D enemyCollider in hitEnemies)
+        {
+            if (enemyCollider.CompareTag("Enemy"))
+            {
+                Enemy enemy = enemyCollider.GetComponent<Enemy>();
+                if (enemy != null)
+                {
+                    float distance = Vector2.Distance(transform.position, enemy.transform.position);
+
+                    if (distance <= innerRange)
+                    {
+                        enemy.TakeDamage(damage);
+                    }
+                    else
+                    {
+                        enemy.TakeDamage(damage * outerDamageMultiplier);
+                    }
+
+                    StartCoroutine(FlashRed(enemy));
+                    StartCoroutine(StunEnemy(enemy, stunDuration));
+                }
+            }
+        }
+    }
+
+    public IEnumerator FlashRed(Enemy enemy)
+    {
+        SpriteRenderer sr = enemy.GetComponent<SpriteRenderer>();
+        if (sr != null)
+        {
+            Color original = sr.color;
+            sr.color = Color.red;
+            yield return new WaitForSeconds(0.1f);
+            sr.color = original;
+        }
+    }
+
+    public IEnumerator StunEnemy(Enemy enemy, float duration)
+    {
+        enemy.Stun(duration);
+        yield return null;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, innerRange);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, outerRange);
+    }
+}
---
+++ Assets/Prefabs/Erin/Animations/test-attacks/Sword.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+
+public class Sword : Weapon
+{
+    public enum PlayerID { Player1, Player2 }
+    public PlayerID owner = PlayerID.Player1;
+
+    public float attackRange = 1.5f;
+    public float knockbackForce = 20f;
+
+    private Collider2D attackCollider;
+
+    private Animator animator;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        attackCollider = GetComponent<Collider2D>();
+        if (attackCollider != null)
+            attackCollider.isTrigger = true;
+    }
+
+    private void Update()
+    {
+        // Separate input based on which player this sword belongs to
+        if (owner == PlayerID.Player1 && Input.GetKeyDown(KeyCode.Q))
+        {
+            Attack();
+            animator.SetTrigger("SwordTrigger");
+        }
+        else if (owner == PlayerID.Player2 && Input.GetKeyDown(KeyCode.U))
+        {
+            Attack();
+            animator.SetTrigger("SwordTrigger");
+        }
+    }
+
+    public override void Attack()
+    {
+        Debug.Log(owner + " sword attack triggered!");
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position, attackRange);
+
+        foreach (Collider2D enemyCollider in hitEnemies)
+        {
+            if (enemyCollider.CompareTag("Enemy"))
+            {
+                Enemy enemy = enemyCollider.GetComponent<Enemy>();
+                if (enemy != null)
+                {
+                    Debug.Log("Sword hit " + enemy.name);
+                    enemy.TakeDamage(damage);
+
+                    Rigidbody2D enemyRb = enemy.GetComponent<Rigidbody2D>();
+                    if (enemyRb != null)
+                    {
+                        Vector2 knockbackDir = (enemy.transform.position - transform.position).normalized;
+                        enemyRb.AddForce(knockbackDir * knockbackForce, ForceMode2D.Impulse);
+                    }
+
+                    StartCoroutine(FlashRed(enemy));
+                }
+            }
+        }
+    }
+
+    private System.Collections.IEnumerator FlashRed(Enemy enemy)
+    {
+        SpriteRenderer enemySprite = enemy.GetComponent<SpriteRenderer>();
+        if (enemySprite)
+        {
+            Color originalColor = enemySprite.color;
+            enemySprite.color = Color.red;
+            yield return new WaitForSeconds(0.1f);
+            enemySprite.color = originalColor;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+}
